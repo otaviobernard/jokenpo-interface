383344e6749ce03ed2f0be0068179c53490c46fb
Otavio Bernardo <otavio9969@gmail.com>
2022-09-05 18:20:10 -0300

    first commit

---
+++ JoKenPo/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace JoKenPo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paperButton = new System.Windows.Forms.Button();
+            this.rockButton = new System.Windows.Forms.Button();
+            this.scissorButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.youLabel = new System.Windows.Forms.Label();
+            this.pcLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // paperButton
+            // 
+            this.paperButton.BackColor = System.Drawing.SystemColors.Control;
+            this.paperButton.BackgroundImage = global::JoKenPo.Properties.Resources.paperHand;
+            this.paperButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.paperButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.paperButton.Location = new System.Drawing.Point(40, 297);
+            this.paperButton.Name = "paperButton";
+            this.paperButton.Size = new System.Drawing.Size(109, 56);
+            this.paperButton.TabIndex = 0;
+            this.paperButton.Text = "Paper";
+            this.paperButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.paperButton.UseVisualStyleBackColor = false;
+            this.paperButton.Click += new System.EventHandler(this.paperButton_Click);
+            // 
+            // rockButton
+            // 
+            this.rockButton.BackgroundImage = global::JoKenPo.Properties.Resources.rockHand;
+            this.rockButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rockButton.Location = new System.Drawing.Point(154, 297);
+            this.rockButton.Name = "rockButton";
+            this.rockButton.Size = new System.Drawing.Size(109, 56);
+            this.rockButton.TabIndex = 1;
+            this.rockButton.Text = "Rock";
+            this.rockButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.rockButton.UseVisualStyleBackColor = true;
+            this.rockButton.Click += new System.EventHandler(this.rockButton_Click);
+            // 
+            // scissorButton
+            // 
+            this.scissorButton.BackgroundImage = global::JoKenPo.Properties.Resources.scissorHand;
+            this.scissorButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.scissorButton.Location = new System.Drawing.Point(268, 297);
+            this.scissorButton.Name = "scissorButton";
+            this.scissorButton.Size = new System.Drawing.Size(109, 56);
+            this.scissorButton.TabIndex = 2;
+            this.scissorButton.Text = "Scissor";
+            this.scissorButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.scissorButton.UseVisualStyleBackColor = true;
+            this.scissorButton.Click += new System.EventHandler(this.scissorButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(314, 98);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(164, 144);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(589, 98);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(164, 144);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // youLabel
+            // 
+            this.youLabel.AutoSize = true;
+            this.youLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.youLabel.Location = new System.Drawing.Point(36, 68);
+            this.youLabel.Name = "youLabel";
+            this.youLabel.Size = new System.Drawing.Size(55, 24);
+            this.youLabel.TabIndex = 6;
+            this.youLabel.Text = "Você";
+            // 
+            // pcLabel
+            // 
+            this.pcLabel.AutoSize = true;
+            this.pcLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pcLabel.Location = new System.Drawing.Point(311, 68);
+            this.pcLabel.Name = "pcLabel";
+            this.pcLabel.Size = new System.Drawing.Size(27, 24);
+            this.pcLabel.TabIndex = 7;
+            this.pcLabel.Text = "IA";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(128, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "VS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(585, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Vencedor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(21, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Placar";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(430, 265);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(323, 114);
+            this.panel1.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(188, 71);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(95, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(221, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "IA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Você";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 401);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pcLabel);
+            this.Controls.Add(this.youLabel);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.scissorButton);
+            this.Controls.Add(this.rockButton);
+            this.Controls.Add(this.paperButton);
+            this.Name = "Form1";
+            this.Text = "JoKenPo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button paperButton;
+        private System.Windows.Forms.Button rockButton;
+        private System.Windows.Forms.Button scissorButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label youLabel;
+        private System.Windows.Forms.Label pcLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ JoKenPo/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JoKenPo
+{
+    public partial class Form1 : Form
+    {
+        List<string> jogos;
+        public Form1()
+        {
+            InitializeComponent();
+            jogos = new List<string>
+            {
+                "paper",
+                "rock",
+                "scissor"
+            };
+        }
+
+        private void scoreControl(string s){
+            if(s == "l")
+            {
+                int iaScore = int.Parse(label7.Text);
+                iaScore = iaScore + 1;
+                label7.Text = iaScore.ToString();
+            }
+            else if(s == "w")
+            {
+                int youScore = int.Parse(label6.Text);
+                youScore = youScore + 1;
+                label6.Text = youScore.ToString();
+            }
+        }
+
+        private void WinImage(string s)
+        {
+            if(s == "l"){
+                pictureBox3.Image = Image.FromFile("images/lose.png");
+            }
+            else if(s == "w")
+            {
+                pictureBox3.Image = Image.FromFile("images/win.png");
+            }
+            else
+            {
+                pictureBox3.Image = Image.FromFile("images/draw.png");
+            }
+
+            scoreControl(s);
+        }
+
+        private void Winner(string you, string iA)
+        {
+            if(you == "rock" && iA == "paper"){
+                WinImage("l");
+            }
+            else if (you == "rock" && iA == "scissor") {
+                WinImage("w");
+            }
+            else if (you == "scissor" && iA == "rock")
+            {
+                WinImage("l");
+            }
+            else if (you == "scissor" && iA == "paper")
+            {
+                WinImage("w");
+            }
+            else if (you == "paper" && iA == "scissor")
+            {
+                WinImage("l");
+            }
+            else if (you == "paper" && iA == "rock")
+            {
+                WinImage("w");
+            }
+            else
+            {
+                WinImage("draw");
+            }
+        }
+
+        private string RandomGenerator()
+        {
+            Random random = new Random();
+            int i = random.Next(jogos.Count);
+            string s = jogos[i];
+            pictureBox2.Image = Image.FromFile("images/" + s + "Hand.png");
+            return s;
+        }
+
+        private void paperButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("images/paperHand.png");
+            string s = RandomGenerator();
+            Winner("paper", s);
+        }
+
+        private void rockButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("images/rockHand.png");
+            string s = RandomGenerator();
+            Winner("rock", s);
+        }
+
+        private void scissorButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("images/scissorHand.png");
+            string s = RandomGenerator();
+            Winner("scissor", s);
+        }
+    }
+}
